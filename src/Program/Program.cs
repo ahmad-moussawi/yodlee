16cfc5f19427dcb4d7e171b0ca4bc817d0b96fb8
Ahmad Moussawi <aymoussawi@gmail.com>
2018-09-07 16:07:13 +0300

    adding Program project

---
+++ src/Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Yodlee;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var config = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText("env.json"));
+
+            Console.WriteLine(config);
+
+            var yodlee = new YodleeApi(
+                (string)config.cobrandName,
+                (string)config.cobrandLogin,
+                (string)config.cobrandPassword,
+                (string)config.userLogin,
+                (string)config.userPassword
+            );
+
+            var response = await yodlee.Accounts();
+
+            Console.WriteLine(response.Json());
+
+        }
+    }
+}
